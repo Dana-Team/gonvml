9cacb2808db70c6dfb3e074a0af3ff5c323b9632
Raz <raz.bne@gmail.com>
2020-12-17 21:31:49 +0200

    init devspace fix

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,23 @@
-package cmd
+package main
+
+import (
+	"fmt"
+	"github.com/Dana-Team/gonvml"
+)
+
+
+func failedMsg(msg string, err error) {
+	fmt.Printf("%s: %+v\n", msg, err)
+}
+
+func main() {
+	gonvml.Init()
+	defer gonvml.Shutdown()
+
+	device, err := gonvml.NewDevice(0)
+	if err != nil {
+		failedMsg("NewDevice", err)
+	} else {
+		fmt.Println(device.UUID)
+	}
+}
